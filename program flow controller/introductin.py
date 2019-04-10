d453bfb4145447a32cbe03b977f1777bae888430
saiandru <nathsaimanishreddy@gmail.com>
2019-04-09 23:15:21 -0700

    new version

---
+++ program flow controller/introductin.py
@@ -0,0 +1,10 @@
+#python workes in blocks of code 
+#principle of python is easy to read
+
+
+for i in range(1,12):
+    #here it moved 4 steps moved is called indenting 
+    print("No {} squared is {} and cubed is {:<4}".format(i,i**2,i**4))
+    print("calculation complete")
+    print("try again")
+    
